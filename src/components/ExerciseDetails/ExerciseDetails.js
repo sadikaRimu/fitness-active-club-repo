42e5f68de9b6245464051a5a691ba0f7bf46e926
sadikaRimu <sadika3046@gmail.com>
2022-09-29 10:44:47 +0600

    personal info design complete

--- src/components/ExerciseDetails/ExerciseDetails.js
+++ src/components/ExerciseDetails/ExerciseDetails.js
@@ -3,8 +3,27 @@ import './ExerciseDetails.css'
 
 const ExerciseDetails = () => {
     return (
-        <div>
-            <h3>Sadika Afrin</h3>
+        <div className='exercise-details-section'>
+            <div>
+                <img src="../../../images/rimu.jpg" alt="" />
+                <h3>Sadika Afrin Rimu</h3>
+            </div>
+            <div className='personal-info'>
+                <div>
+                    <div className='single-info'>
+                        <h3>75</h3><small>kg</small>
+                    </div>
+                    <small className='measure-unit'>Weight</small>
+                </div>
+                <div className='height'>
+                    <h3>6.5</h3>
+                    <small className='measure-unit'>Height</small>
+                </div>
+                <div className='age'>
+                    <div className='single-info'><h3>25</h3><small>years</small></div>
+                    <small className='measure-unit'>Age</small>
+                </div>
+            </div>
             <div>
                 <h4>Add A Break</h4>
                 <div className='break-time-container'>
